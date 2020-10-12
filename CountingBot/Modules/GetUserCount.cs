f6cfab113d7d0dd3ece96b13605785733162b11c
Benjamin Hall <hallb1016@gmail.com>
2020-10-12 17:28:35 -0400

    Updated getcount to use medals

--- CountingBot/Modules/GetUserCount.cs
+++ CountingBot/Modules/GetUserCount.cs
@@ -26,12 +26,19 @@ namespace CountingBot.Modules
             Task<int> count = countingDatabase.UserCounts.GetUserCountAsync(user);
 
             List<(SocketGuildUser user, int count)> userCounts = await countingDatabase.UserCounts.GetAllUserCountsAsync(Context.Guild);
-            int userRank = 1 + userCounts.IndexOf((user, await count));
+            int rank = 1 + userCounts.IndexOf((user, await count));
+            string rankString = rank switch
+            {
+                1 => ":first_place:",
+                2 => ":second_place:",
+                3 => ":third_place:",
+                _ => rank.ToString()
+            };
 
             EmbedBuilder embed = new EmbedBuilder()
                 .WithColor(SecurityInfo.botColor)
                 .WithDescription($"{user.Mention} has sent {await count} messages in the counting channel.\n" +
-                    $"Rank: {userRank}");
+                    $"Rank: {rankString}");
 
             await Context.Channel.SendMessageAsync(embed: embed.Build());
         }
